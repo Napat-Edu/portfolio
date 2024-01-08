550c06b0e1a628e4fe19ea48bcd0007c8d2badfe
Napat Wareedee <omegyzr@gmail.com>
2024-01-08 22:25:43 +0700

    adjust experience section layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import Card from "./components/Card"
 function App() {
 
   return (
-    <main className="font-medium">
+    <main className="font-medium text-dark-blue">
       <section className="min-h-64 flex">
         <div className="flex flex-row justify-between items-center align-middle w-7/12 m-auto">
 
-          <div className="flex flex-col text-dark-blue">
+          <div className="flex flex-col">
             <p>Hello world, I am</p>
             <p className="text-5xl font-bold mb-4">Napat Wareedee</p>
             <p>Dreams to become software engineer</p>
@@ -19,7 +19,7 @@ function App() {
       </section>
 
       <section className="w-7/12 mx-auto my-4">
-        <h2 className="font-semibold text-2xl">Projects</h2>
+        <h2 className="font-bold text-2xl">Projects</h2>
         <div className="grid grid-cols-3 grid-rows-2 gap-4 my-2">
           <Card
             title="Compath (now)"
@@ -38,6 +38,7 @@ function App() {
             description="Entertainment Dubbing App for blind person (Android)"
             bannerImage="overvoice-banner.png"
           >
+
           </Card>
           <Card
             title="TyBunTee (2023)"
@@ -55,27 +56,32 @@ function App() {
       </section>
 
       <section className="w-7/12 mx-auto my-4">
-        <h2 className="font-semibold text-2xl">Experiences</h2>
-        <div className="flex flex-row mt-2">
-          <img src="ais-icon.png" alt="AIS icon" className="w-[45px] h-[20px] mt-2 mr-2" />
-          <div className="flex flex-col">
-            <div className="mb-2">
-              <h3>Full-Stack Developer</h3>
+        <h2 className="font-bold text-2xl">Experiences</h2>
+        <div className="flex flex-col mt-2">
+
+          <div className="flex flex-row mb-2">
+            <img className="w-[55px] h-[20px] mt-2 mr-2" src="ais-icon.png" alt="AIS icon" />
+            <div>
+              <h3 className="font-bold text-lg">Full-Stack Developer</h3>
               <p>Advanced Info Service Public Company Limited (AIS) · Internship</p>
               <p>Jun - Aug 2023 · 10 weeks</p>
             </div>
-            <p>- cooperate in development team from AIS x ODDS to redesign and develop the device selling web application for AIS shop.</p>
-            <p>- responsible to develop the user interface with Angular and restructure the API with Express.</p>
           </div>
+
+          <div>
+            <p className="font-normal">- cooperate in development team from AIS x ODDS to redesign and develop the device selling web application for AIS shop.</p>
+            <p className="font-normal">- responsible to develop the user interface with Angular and restructure API with Express.</p>
+          </div>
+
         </div>
       </section>
 
       <section className="w-7/12 mx-auto my-4">
-        <h2 className="font-semibold text-2xl">Awards</h2>
+        <h2 className="font-bold text-2xl">Awards</h2>
       </section>
 
       <section className="w-7/12 mx-auto my-4">
-        <h2 className="font-semibold text-2xl">Skills</h2>
+        <h2 className="font-bold text-2xl">Skills</h2>
       </section>
 
     </main>
