a56cdd0a038a1504420b41a1b6d11fe98caa118b
Napat-Edu <omegyzr@gmail.com>
2024-01-11 17:53:32 +0700

    adjust dialog layout

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -8,6 +8,7 @@ interface IDialogProps {
 }
 
 function Dialog(props: IDialogProps) {
+    const backgroundShade = ["bg-blue-400", "bg-purple-400"];
 
     return (
         <div className="relative z-10">
@@ -15,29 +16,34 @@ function Dialog(props: IDialogProps) {
 
             <div className="fixed inset-0 z-10 w-screen">
                 <div className="flex min-h-full items-end justify-center p-4">
-                    <div className="rounded-lg bg-white w-3/4 lg:w-2/3 h-[80dvh] lg:h-[90dvh] my-auto p-2 overflow-y-auto">
+                    <div className="flex flex-col justify-between rounded-lg bg-white w-3/4 lg:w-2/3 h-[80dvh] lg:h-[85dvh] my-auto p-2 overflow-y-auto">
 
-                        <div className="relative mb-2">
-                            <img className="w-full h-64 object-cover object-center" src={props.info.bannerImage} alt="overvoice-banner-dialog" />
-                            <button className="absolute top-1 right-1 w-10 h-10 bg-white rounded-full" onClick={props.closeModal} type="button">
-                                <img src="close.png" alt="close-button" />
-                            </button>
-                        </div>
-                        <h3 className="font-bold text-4xl">{props.info.title}</h3>
+                        <div>
+                            <div className="relative mb-2">
+                                <img className="w-full h-64 object-cover object-center" src={props.info.bannerImage} alt="overvoice-banner-dialog" />
+                                <button className="absolute top-1 right-1 w-10 h-10 bg-white rounded-full" onClick={props.closeModal} type="button">
+                                    <img src="close.png" alt="close-button" />
+                                </button>
+                            </div>
+                            <h3 className="font-bold text-4xl">{props.info.title}</h3>
 
-                        <p className="mt-2" dangerouslySetInnerHTML={{ __html: props.info.detail }}></p>
+                            <p className="mt-2" dangerouslySetInnerHTML={{ __html: props.info.detail }}></p>
 
-                        <div className="mt-2">
-                            sample videos
+                            {
+                                props.info.sampleVideos ?
+                                    <div className="mt-2">
+                                        sample videos
+                                    </div> :
+                                    <></>
+                            }
                         </div>
 
                         <div className="flex flex-row gap-4 mt-2">
                             {
                                 props.info.sourceCode ?
                                     props.info.sourceCode.map((code, idx) => {
-                                        const shade = 300 + (idx * 100);
                                         return (
-                                            <a key={"src-code-" + idx} href={code} target="_blank" className={`rounded-md bg-slate-${shade} px-2 py-1`}>
+                                            <a key={"src-code-" + idx} href={code} target="_blank" className={`rounded-md ${backgroundShade[idx]} px-2 py-1`}>
                                                 <img className="w-8 h-8" src="/icons/code-icon.png" alt="source-code" />
                                             </a>
                                         );
