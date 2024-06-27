e1750f4c6a7b0224ccd17f06bf43c8d31bbb1d32
Napat Wareedee <omegyzr@gmail.com>
2024-06-27 22:26:19 +0700

    add tailwindcss-animate

--- src/sections/IntroSection.tsx
+++ src/sections/IntroSection.tsx
@@ -10,21 +10,18 @@ export default function IntroSection() {
         <section>
             <div className="flex flex-col gap-8 lg:flex-row justify-between items-center lg:w-7/12 mx-auto">
 
-                <div className="flex flex-col items-center lg:items-start gap-2">
+                <div className="flex flex-col items-center lg:items-start gap-2 animate-fade-right animate-once animate-ease-out">
                     <p>Hello world, I am</p>
                     <h1>Napat Wareedee</h1>
                     <p>Dreams to become software engineer</p>
                     <ContactList />
                 </div>
 
-                <div className="relative w-36 h-36">
-                    <img
-                        src="pat-profile.jpg"
-                        onClick={rickRoll}
-                        className="absolute z-10 inset-0 border-0 rounded-full w-36 h-36 cursor-pointer object-cover"
-                    />
-                    <span className="absolute z-0 animate-ping inset-0 h-full w-full rounded-full bg-light-orange opacity-75"></span>
-                </div>
+                <img
+                    src="pat-profile.jpg"
+                    onClick={rickRoll}
+                    className="border-0 rounded-full w-36 h-36 cursor-pointer object-cover animate-fade-left animate-once animate-ease-out"
+                />
 
             </div>
         </section>
