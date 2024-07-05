edb14bda0b5be14e206b18faaf8257d6e3970034
Napat Wareedee <omegyzr@gmail.com>
2024-07-06 01:20:35 +0700

    update card container

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,42 +1,89 @@
-import { useState } from "react";
-import Dialog from "./Dialog";
-import { ICardLists } from "../items/CardList";
+import { Children, cloneElement, isValidElement, ReactNode, ReactElement, useState } from 'react';
+import Badge from './Badge';
 
-interface ICardList {
-    info: ICardLists;
+interface ICard {
+    title: string;
+    description: string;
+    image: string;
+    tools: string[];
+    cardIdx?: number;
+    isSelected?: boolean;
+    className?: string;
+    onClick?: (index: number) => void;
 }
 
-function Card(props: ICardList) {
+export function Card(props: ICard) {
+    const unselectedStyle = 'opacity-50 shadow-none hover:shadow-none hover:outline-2 hover:border-slate-400 translate-y-3';
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
-
-    const closeModal = () => {
-        setIsModalOpen(false);
+    const handleCardClick = (index: number) => {
+        if (props.onClick) {
+            props.onClick(index);
+        }
     };
 
     return (
-        <div className="fade-in min-w-56 max-w-64 rounded-lg overflow-hidden shadow-md flex flex-col justify-between">
-            <div>
-                <img className="w-full h-44 object-cover object-center" src={props.info.bannerImage} alt={"banner-" + props.info.title} />
-                <div className="px-6 py-2">
-                    <div className="font-bold text-xl mb-2 truncate">{props.info.title}</div>
-                    <p className="text-gray-700 text-base">{props.info.description}</p>
+        <div
+            className={`
+                w-64 h-full border rounded-2xl overflow-hidden 
+                shadow-md hover:shadow-xl transition-shadow
+                ${props.className || ''}
+                ${props.isSelected ? '' : unselectedStyle}
+            `}
+            onClick={() => {
+                handleCardClick(props.cardIdx!);
+            }}
+        >
+            <img
+                src={`${props.image}`}
+                alt="project banner image"
+                className={`w-full h-1/2 object-cover object-center`}
+            />
+            <div className='flex flex-col gap-1 p-4'>
+                <h3>{props.title}</h3>
+                <div className='flex flex-wrap gap-1'>
+                    {
+                        props.tools.map((tool) => {
+                            return <Badge key={`${props.title}-${tool}`}>{tool}</Badge>
+                        })
+                    }
+                </div>
+                <p>{props.description}</p>
+                <div className='flex gap-2 mt-4'>
+                    <button className='min-w-fit px-2'>{`</>`}</button>
+                    <button className='w-4/5'>Visit</button>
                 </div>
             </div>
-
-            {isModalOpen ?
-                <Dialog
-                    closeModal={closeModal}
-                    info={props.info} /> :
-                <></>}
-
-            <button className="bg-primary rounded-lg text-white px-4 py-2 mx-4 mt-2 mb-4" onClick={openModal}>More Details</button>
         </div>
     );
 }
 
-export default Card;
+interface ICardContainer {
+    children: ReactNode;
+}
+
+export function CardContainer(props: ICardContainer) {
+    const [focusCard, serFocusCard] = useState<number>(0);
+
+    const handleClick = (index: number) => {
+        serFocusCard(index);
+    };
+
+    return (
+        <div
+            className={`
+                flex justify-center gap-10 min-h-fit
+            `}
+        >
+            {Children.map(props.children, (child, childIdx) => {
+                if (isValidElement(child)) {
+                    return cloneElement(child as ReactElement<ICard>, {
+                        onClick: handleClick,
+                        cardIdx: childIdx,
+                        isSelected: focusCard == childIdx
+                    });
+                }
+                return child;
+            })}
+        </div>
+    );
+}
