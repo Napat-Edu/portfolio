d6144f4b5b3df9b65a3d1384180c0be61fa732d1
Napat Wareedee <omegyzr@gmail.com>
2024-07-07 22:42:42 +0700

    add theme props

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,15 +1,27 @@
 import { ReactNode } from "react";
 
+type ThemeType = 'light' | 'dark';
+
 interface IBadge {
+    theme?: ThemeType;
     children?: ReactNode;
 }
 
 export default function Badge(props: IBadge) {
+    const handleTheme = () => {
+        let themeStyle = '';
+        if (props.theme == 'dark') {
+            themeStyle = 'text-white bg-zinc-800 hover:bg-zinc-500';
+        } else {
+            themeStyle = 'bg-white hover:bg-slate-100 ';
+        }
+        return themeStyle;
+    };
 
     return (
         <div className={`
             border-2 rounded-2xl w-fit px-2
-            text-sm hover:bg-slate-100 
+            text-sm ${handleTheme()}
         `}>{props.children}</div>
     );
 }
