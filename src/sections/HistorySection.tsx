d959576af8a1f476846df855c9bce3ce1b9f8b2f
Napat Wareedee <omegyzr@gmail.com>
2024-07-18 22:36:47 +0700

    update responsive margin

--- src/sections/HistorySection.tsx
+++ src/sections/HistorySection.tsx
@@ -5,8 +5,8 @@ export default function HistorySection() {
             <h2 className='text-center'>What I have done</h2>
 
             <div className="flex flex-col divide-y-2 mt-2">
-                <div className="flex gap-4 py-4">
-                    <img className="ww-80 h-80 rounded-md flex-shrink-0 flex-grow-0" src="ais-intern.jpg" alt="AIS intern history" />
+                <div className="flex flex-col lg:flex-row gap-4 py-4 mx-10 lg:mx-0">
+                    <img className="w-80 h-80 rounded-md flex-shrink-0 flex-grow-0 mx-auto" src="ais-intern.jpg" alt="AIS intern history" />
                     <div className="flex flex-col gap-2">
                         <img className="w-14 h-auto mt-2 mr-2" src="ais-icon.png" alt="AIS icon" />
                         <h3>Full-Stack Developer</h3>
@@ -19,8 +19,8 @@ export default function HistorySection() {
                     </div>
                 </div>
 
-                <div className="flex flex-row-reverse gap-4 py-4">
-                    <img className="ww-80 h-80 rounded-md flex-shrink-0 flex-grow-0" src="newfounder.jpg" alt="Newfounder winner history" />
+                <div className="flex flex-col lg:flex-row-reverse gap-4 py-4 mx-10 lg:mx-0">
+                    <img className="w-80 h-80 rounded-md flex-shrink-0 flex-grow-0 mx-auto" src="newfounder.jpg" alt="Newfounder winner history" />
                     <div className="flex flex-col gap-2">
                         <img className="w-14 h-auto mt-2 mr-2" src="newfounder-icon.png" alt="Newfounder icon" />
                         <h3>The Winner & Best Design in New Founder 2023 by Young Entrepreneur Assembly Hub (YEAH)</h3>
