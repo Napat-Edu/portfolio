def00cd21aa8aff969b0a3d0f2e7b3a6436ced4d
Napat Wareedee <omegyzr@gmail.com>
2024-07-21 17:28:25 +0700

    update primary text fot history

--- src/sections/HistorySection.tsx
+++ src/sections/HistorySection.tsx
@@ -9,8 +9,8 @@ export default function HistorySection() {
                     <img className="w-80 h-80 rounded-md flex-shrink-0 flex-grow-0 mx-auto" src="ais-intern.jpg" alt="AIS intern history" />
                     <div className="flex flex-col gap-2">
                         <img className="w-14 h-auto mt-2 mr-2" src="ais-icon.png" alt="AIS icon" />
-                        <h3>Full-Stack Developer</h3>
-                        <p>Advanced Info Service Public Company Limited (AIS) · Internship</p>
+                        <h3>Advanced Info Service Public Company Limited (AIS)</h3>
+                        <p>Full-Stack Developer Internship</p>
                         <p>Jun - Aug 2023 · 10 weeks</p>
                         <ul className="list-disc list-outside ms-4">
                             <li>cooperate in development team from <strong>AIS x ODDS</strong> to redesign and develop the device selling web application for AIS shop.</li>
